007b20a1c531d1eeef79bafd69f4bc44ec8d1dc0
Wonsup Yoon <pusnow@kaist.ac.kr>
2023-02-13 23:44:51 +0900

    Add font-elice-digital-coding

---
+++ Casks/font-elice-digital-coding.rb
@@ -0,0 +1,11 @@
+cask "font-elice-digital-coding" do
+  version :latest
+  sha256 :no_check
+
+  url "https://elice.io/font/EliceDigitalCoding_OTF.zip"
+  name "Elice Digital Coding"
+  homepage "https://font.elice.io/"
+
+  font "EliceDigitalCodingOTF_Regular.otf"
+  font "EliceDigitalCodingOTF_Bold.otf"
+end
